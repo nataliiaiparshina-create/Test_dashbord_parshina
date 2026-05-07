bdf9a72957e0d35501814c3bf0fc08c37e786a6c
Natalia <nataliia.i.parshina@gmail.com>
2026-05-07 10:08:12 +0300

    fix: eslint warning

--- src/components/AIPanel.js
+++ src/components/AIPanel.js
@@ -37,7 +37,7 @@ export default function AIPanel({ data }) {
     setLoading(false);
   };
 
-  useEffect(() => { analyze(); }, []);
+useEffect(() => { analyze(); }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div style={{
